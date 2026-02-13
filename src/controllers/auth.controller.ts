f1ee564bf6910bd6ae13824854b606988f2125c1
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2026-02-13 12:00:00 +0700

    feat: add login endpoint
    - user schema toJSON method to hide password
    - auth login route, controller, and type

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as Yup from "yup";
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
 
 type TRegister = {
     fullname: string;
@@ -8,6 +9,11 @@ type TRegister = {
     email: string;
     password: string;
     passwordConfirmation: string;
+};
+
+type TLogin = {
+    identifier: string;
+    password: string;
 };
 
 const registerValidationSchema = Yup.object({
@@ -36,6 +42,41 @@ export default {
             res.status(200).json({
                 message: "Registration successful",
                 data: result,
+            });
+        } catch (error) {
+            const err = error as unknown as Error;
+            res.status(400).json({ message: err.message, data: null });
+        }
+    },
+
+    async login(req: Request, res: Response) {
+        const { identifier, password } = req.body as unknown as TLogin;
+        try {
+            // ambil data user brdasarkan 'identifier' => email dan username
+            const userByIdentifier = await UserModel.findOne({
+                $or: [
+                    {
+                        email: identifier,
+                    },
+                    {
+                        username: identifier,
+                    },
+                ],
+            });
+
+            if (!userByIdentifier) {
+                return res.status(403).json({ message: "User not found", data: null });
+            }
+            // validasi password
+            const validatePassword: boolean = encrypt(password) === userByIdentifier.password;
+
+            if (!validatePassword) {
+                return res.status(403).json({ message: "Invalid password", data: null });
+            }
+
+            res.status(200).json({
+                message: "Login successful",
+                data: userByIdentifier,
             });
         } catch (error) {
             const err = error as unknown as Error;
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,5 +4,6 @@ import authController from "../controllers/auth.controller";
 const router = expess.Router();
 
 router.get("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
 
 export default router;
